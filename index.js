4066801807e452fb1f2250718642a0c97eb2e356
HassanKhan8264 <HF610061@GMAIL.COM>
2024-05-31 21:55:49 +0500

    some ts practice

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+console.log('typescript code');
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(2, 2));
+let longText = 'Lon Sentence';
+let shortText = longText.slice(0, 10);
+console.log(shortText);
+let str1 = 'sent one';
+let str2 = 'sent two';
+let areEqual = false;
+if (str1 === str2) {
+    areEqual = true;
+}
+console.log(areEqual);
+let price = 20;
+let product = `laptop`;
+let priceAndProduct = `product ${product} is price at ${price}`;
+console.log(priceAndProduct);
--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
 console.log('typescript code');
+
+
+const sum = (a: number, b: number) => {
+    return a + b;
+}
+console.log(sum(2, 2));
+
+
+let longText: string = 'Lon Sentence'
+let shortText = longText.slice(0, 10)
+console.log(shortText);
+
+
+let str1: string = 'sent one'
+let str2: string = 'sent two'
+let areEqual: boolean = false
+
+if (str1 === str2) {
+    areEqual = true
+}
+console.log(areEqual);
+
+let price: number = 20
+let product: string = `laptop`
+let priceAndProduct = `product ${product} is price at ${price}`
+
+console.log(priceAndProduct);
